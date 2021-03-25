e02d807852643a538321a5631c33ce48f17f44d1
ji sun lee <ellijs@hotmail.com>
2021-03-25 03:54:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
-    if (findFn(array[i]) === false) {
-      return null;
-    } else {
-      return array[i];
+    if (findFn(array[i]) === true) {
+       return array[i]
     }
   }
+  return null
 }
